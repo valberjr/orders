e71461a8ace5b2f14a926223e55b2b5bac185a85
valberjr <vjunior@skiff.com>
2023-10-06 16:14:03 -0300

    refactor: removed unnecessary exceptions

--- orders-api/src/main/java/com/example/msorder/controllers/ApplicationControllerAdvice.java
+++ orders-api/src/main/java/com/example/msorder/controllers/ApplicationControllerAdvice.java
@@ -1,11 +1,7 @@
 package com.example.msorder.controllers;
 
 import com.example.msorder.dtos.ExceptionResponseDto;
-import com.example.msorder.exceptions.InvalidJwtAuthenticationException;
-import io.jsonwebtoken.JwtException;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,30 +12,6 @@ public class ApplicationControllerAdvice {
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionResponseDto handleIllegalArgumentException(IllegalArgumentException e) {
-        return new ExceptionResponseDto(e.getMessage());
-    }
-
-    @ExceptionHandler(BadCredentialsException.class)
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    public ExceptionResponseDto handleBadCredentialsException(Exception e) {
-        return new ExceptionResponseDto(e.getMessage());
-    }
-
-    @ExceptionHandler(DisabledException.class)
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    public ExceptionResponseDto handleDisabledException(Exception e) {
-        return new ExceptionResponseDto(e.getMessage());
-    }
-
-    @ExceptionHandler(JwtException.class)
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    public ExceptionResponseDto handleJwtException(Exception e) {
-        return new ExceptionResponseDto(e.getMessage());
-    }
-
-    @ExceptionHandler(InvalidJwtAuthenticationException.class)
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    public ExceptionResponseDto handleInvalidJwtAuthenticationException(Exception e) {
         return new ExceptionResponseDto(e.getMessage());
     }
 
