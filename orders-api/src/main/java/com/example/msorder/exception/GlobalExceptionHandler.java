a2c8fb1938452895bce30dbf288db05115ddd60b
valberjr <vjunior@skiff.com>
2023-10-26 11:55:33 -0300

    refactor: removed unused methods

--- orders-api/src/main/java/com/example/msorder/exception/GlobalExceptionHandler.java
+++ orders-api/src/main/java/com/example/msorder/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.msorder.exception;
 
-import com.example.msorder.util.DateUtil;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
@@ -11,9 +10,8 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import java.time.Instant;
 
 
 @RestControllerAdvice
@@ -21,47 +19,17 @@ import java.time.Instant;
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     /**
-     * Spring Security Exceptions
+     * Spring Security Authentication Exceptions
      */
-
-    @ExceptionHandler(AccessDeniedException.class)
-    public ProblemDetail handleAccessDeniedException(AccessDeniedException e) {
-        log.error("An AccessDeniedException has occurred", e);
-        var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, e.getMessage());
-        problemDetail.setProperty("timestamp", DateUtil.toISOInstant(Instant.now()));
-        return problemDetail;
-    }
-
-    @ExceptionHandler(BadCredentialsException.class)
-    public ProblemDetail handleAuthenticationException(BadCredentialsException e) {
-        log.error("An BadCredentialsException has occurred", e);
-        var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, e.getMessage());
-        problemDetail.setProperty("timestamp", DateUtil.toISOInstant(Instant.now()));
-        return problemDetail;
-    }
-
-    @ExceptionHandler(ExpiredJwtException.class)
-    public ProblemDetail handleExpiredJwtException(ExpiredJwtException e) {
-        log.error("An ExpiredJwtException has occurred", e);
-        var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, e.getMessage());
-        problemDetail.setProperty("timestamp", DateUtil.toISOInstant(Instant.now()));
-        return problemDetail;
-    }
-
-    @ExceptionHandler(SignatureException.class)
-    public ProblemDetail handleSignatureException(SignatureException e) {
-        log.error("An SignatureException has occurred", e);
-        var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, e.getMessage());
-        problemDetail.setProperty("timestamp", DateUtil.toISOInstant(Instant.now()));
-        return problemDetail;
-    }
-
-    @ExceptionHandler(UsernameNotFoundException.class)
-    public ProblemDetail handleSignatureException(UsernameNotFoundException e) {
-        log.error("An UsernameNotFoundException has occurred", e);
-        var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, e.getMessage());
-        problemDetail.setProperty("timestamp", DateUtil.toISOInstant(Instant.now()));
-        return problemDetail;
+    @ExceptionHandler({AccessDeniedException.class,
+            BadCredentialsException.class,
+            ExpiredJwtException.class,
+            SignatureException.class,
+            UsernameNotFoundException.class
+    })
+    public ProblemDetail handleAuthenticationException(RuntimeException e, WebRequest request) {
+        log.error("An AuthenticationException has occurred", e);
+        return ProblemDetail.forStatusAndDetail(HttpStatus.UNAUTHORIZED, e.getMessage());
     }
 
 }
